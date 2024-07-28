7e2d5efb3dc6e1e5ce2631b7229f1ee9137c68a3
RElackiya <RELACKIYA96@GMAIL.COM>
2024-07-28 17:33:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var sidenav=document.querySelector(".side-menubar")
+
+function showNavbar(){
+sidenav.style.left="0"
+}
+function closeNavbar(){
+sidenav.style.left="-60%"
+}
+var offer=document.querySelector(".flatoffer")
+document.getElementById("closeoffer").addEventListener("click",
+
+
+function (){
+    offer.style.display="none"
+}
+)
